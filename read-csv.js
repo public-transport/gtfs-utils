1475741f0cf50fdb75d5a400c8d2d9eb4b7c4cf5
Jannis R <mail@jannisr.de>
2018-02-24 15:45:18 +0100

    read-csv: emit error before end, 0.4.1
    This is an awful hack. Either there's a way I don't know, or there are
    only clumsy ways to do this with Node Streams.

--- read-csv.js
+++ read-csv.js
@@ -9,9 +9,24 @@ const readCsv = (src) => {
 	const one = fs.createReadStream(src)
 	const two = stripBomStream()
 	const three = parseCsv()
+	// streams are awful
+	one.on('error', (err) => {
+		// .destroy(err) emits `error` async, but we need it right now
+		// todo: find a proper solution
+		one.unpipe(two)
+		two.unpipe(three)
+		three.emit('error', err)
+		two.end()
+		three.end()
+	})
+	two.on('error', (err) => {
+		// .destroy(err) emits `error` async, but we need it right now
+		// todo: find a proper solution
+		two.unpipe(three)
+		three.emit('error', err)
+		three.end()
+	})
 	one.pipe(two).pipe(three)
-	one.once('error', err => two.destroy(err))
-	two.once('error', err => three.destroy(err))
 	return three
 }
 
