5cf304ca1bbbb885a4ed7b0b4fd14650bd91bc07
Jannis R <mail@jannisr.de>
2020-06-04 18:35:36 +0200

    read-csv: use pump

--- read-csv.js
+++ read-csv.js
@@ -1,34 +1,17 @@
 'use strict'
 
 const fs = require('fs')
-const path = require('path')
+const pump = require('pump')
 const stripBomStream = require('strip-bom-stream')
 const parseCsv = require('csv-parser')
 
-// todo: use pump
 const readCsv = (src) => {
-	const one = fs.createReadStream(src)
-	const two = stripBomStream()
-	const three = parseCsv()
-	// streams are awful
-	one.on('error', (err) => {
-		// .destroy(err) emits `error` async, but we need it right now
-		// todo: find a proper solution
-		one.unpipe(two)
-		two.unpipe(three)
-		three.emit('error', err)
-		two.end()
-		three.end()
-	})
-	two.on('error', (err) => {
-		// .destroy(err) emits `error` async, but we need it right now
-		// todo: find a proper solution
-		two.unpipe(three)
-		three.emit('error', err)
-		three.end()
-	})
-	one.pipe(two).pipe(three)
-	return three
+	return pump(
+		fs.createReadStream(src),
+		stripBomStream(),
+		parseCsv(),
+		() => {}, // no-op cb
+	)
 }
 
 module.exports = readCsv
