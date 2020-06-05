4eed417241c29e7b3ad12aa479ef93fb62d5d228
Jannis R <mail@jannisr.de>
2020-06-05 16:40:56 +0200

    read-stops: use async in-memory store :boom:

--- read-stops.js
+++ read-stops.js
@@ -1,52 +1,54 @@
 'use strict'
 
-const debug = require('debug')('gtfs-utils:read-stops')
+const inMemoryStore = require('./lib/in-memory-store')
+const reduce = require('./lib/reduce')
 
 const noFilter = () => true
 
-const readStops = (readFile, filter = noFilter) => {
-	return new Promise((resolve, reject) => {
-		const data = readFile('stops')
-		data.once('error', (err) => {
-			reject(err)
-			data.destroy(err)
-		})
+const readStops = async (readFile, filter = noFilter, opt = {}) => {
+	const {
+		createStore,
+	} = {
+		createStore: inMemoryStore,
+		...opt,
+	}
 
-		const stops = Object.create(null) // by ID
-		data.on('data', (s) => {
-			if (!filter(s)) return;
-			// todo: support these
-			if (s.location_type === '3' || s.location_type === '4') return;
+	const stops = createStore()
+	const processStop = async (s) => {
+		if (!filter(s)) return;
+		// todo: support these
+		if (s.location_type === '3' || s.location_type === '4') return;
 
-			if (s.location_type === '1') {
-				// todo [breaking]: rename to `platforms`
-				s = {...s, child_stops: []}
-			}
-			stops[s.stop_id] = s
-		})
+		if (s.location_type === '1') {
+			s = {...s, child_stops: []}
+		}
+		await stops.set(s.stop_id, s)
+	}
 
-		data.once('end', (err) => {
-			if (err) return;
+	const file = readFile('stops')
+	await reduce('stops', file, stops, processStop)
 
-			for (const id in stops) {
-				const stop = stops[id]
-				const isStop = (
-					!('location_type' in stop) ||
-					stop.location_type === '0'
-				)
-				if (
-					isStop &&
-					stop.parent_station &&
-					(stop.parent_station in stops)
-				) {
-					const station = stops[stop.parent_station]
-					station.child_stops.push(id)
-				}
-			}
+	for await (const [id, stop] of stops.entries()) {
+		// continue if it's not a stop
+		// todo: add entrances to their parents
+		// todo: add boarding areas to their parents
+		// todo: add generic nodes to their parents?
+		if (
+			('location_type' in stop)
+			&& stop.location_type !== '0'
+		) continue
 
-			setImmediate(resolve, stops)
-		})
-	})
+		// continue if if the stop doesn't have a parent station
+		if (!stop.parent_station) continue
+		const station = await stops.get(stop.parent_station)
+		if (!station) continue
+
+		// add to stop to station.child_stops
+		station.child_stops.push(id)
+		await stops.set(stop.parent_station, station)
+	}
+
+	return stops
 }
 
 module.exports = readStops
