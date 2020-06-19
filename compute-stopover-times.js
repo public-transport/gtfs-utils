ad0d8a3f589c85f602daa3bef51948187a2b0c7c
Jannis R <mail@jannisr.de>
2020-06-19 14:59:56 +0200

    compute-stopover-times: use read-and-sort-stop-times :boom:

--- compute-stopover-times.js
+++ compute-stopover-times.js
@@ -5,22 +5,29 @@ const {DateTime} = require('luxon')
 const pump = require('pump')
 
 const inMemoryStore = require('./lib/in-memory-store')
+const readAndSortStopTimes = require('./lib/read-and-sort-stop-times')
 const readServicesAndExceptions = require('./read-services-and-exceptions')
-const readTrips = require('./read-trips')
+const processFile = require('./lib/process-file')
 const parseTime = require('./parse-time')
-const errorsWithRow = require('./lib/errors-with-row')
+const resolveTime = require('./lib/resolve-time')
 
 const isObj = o => 'object' === typeof o && o !== null && !Array.isArray(o)
 
-// todo: stopover.stop_timezone
-const computeStopoverTimes = (readFile, filters, timezone, opt = {}) => {
+// todo: respect stopover.stop_timezone & agency.agency_timezone
+const computeStopoverTimes = async function* (readFile, timezone, filters = {}, opt = {}) {
 	if ('function' !== typeof readFile) {
 		throw new Error('readFile must be a function.')
 	}
+
+	if ('string' !== typeof timezone || !timezone) {
+		throw new Error('timezone must be a non-empty string.')
+	}
+
 	if (!isObj(filters)) throw new Error('filters must be an object.')
 	filters = {
 		trip: () => true,
 		stopover: () => true,
+		frequenciesRow: () => true,
 		...filters,
 	}
 	if ('function' !== typeof filters.trip) {
@@ -29,9 +36,10 @@ const computeStopoverTimes = (readFile, filters, timezone, opt = {}) => {
 	if ('function' !== typeof filters.stopover) {
 		throw new Error('filters.stopover must be a function.')
 	}
-	const {
-		stopover: stopoverFilter,
-	} = filters
+	if ('function' !== typeof filters.frequenciesRow) {
+		throw new Error('filters.frequenciesRow must be a function.')
+	}
+
 	const {
 		createStore,
 	} = {
@@ -39,66 +47,79 @@ const computeStopoverTimes = (readFile, filters, timezone, opt = {}) => {
 		...opt,
 	}
 
-	let services, trips
+	const {
+		stopsByTripId,
+		arrivalsByTripId,
+		departuresByTripId,
+		headwayBasedStarts, headwayBasedEnds, headwayBasedHeadways,
+	} = await readAndSortStopTimes(readFile, filters, {createStore})
 
-	const processStopover = (s, _, cb) => {
-		;(async () => {
-			if (!stopoverFilter(s)) return;
+	const services = await readServicesAndExceptions(readFile, timezone, filters)
 
-			const t = await trips.get(s.trip_id)
-			if (!t) return;
-			const [serviceId, routeId] = t
-			const days = await services.get(serviceId)
-			if (!days) return;
+	const trips = createStore()
+	const processTrip = async (t) => {
+		if (!filters.trip(t)) return;
+		await trips.set(t.trip_id, [t.service_id, t.route_id])
+	}
+	await processFile('trips', readFile('trips'), processTrip)
 
-			const arr = parseTime(s.arrival_time)
-			const dep = parseTime(s.departure_time)
+	for await (const tripId of stopsByTripId.keys()) {
+		const t = await trips.get(tripId)
+		if (!t) continue
+		const [serviceId, routeId] = t
+		const days = await services.get(serviceId)
+		if (!days) continue
 
-			for (let day of days) {
-				const d = DateTime.fromMillis(day * 1000, {zone: timezone})
-				out.push({
-					stop_id: s.stop_id,
-					trip_id: s.trip_id,
+		const [
+			stops, arrs, deps,
+			hwStarts, hwEnds, hwHeadways,
+		] = await Promise.all([
+			stopsByTripId.get(tripId),
+			arrivalsByTripId.get(tripId),
+			departuresByTripId.get(tripId),
+			headwayBasedStarts.get(tripId),
+			headwayBasedEnds.get(tripId),
+			headwayBasedHeadways.get(tripId),
+		])
+
+		for (const day of days) {
+			// schedule-based
+			for (let i = 0; i < stops.length; i++) {
+				yield {
+					stop_id: stops[i],
+					trip_id: tripId,
 					service_id: serviceId,
 					route_id: routeId,
-					sequence: s.stop_sequence,
 					start_of_trip: day,
-					arrival: d.plus(arr) / 1000 | 0,
-					departure: d.plus(dep) / 1000 | 0
-				})
+					arrival: resolveTime(timezone, day, arrs[i]),
+					departure: resolveTime(timezone, day, deps[i]),
+				}
 			}
-		})()
-		.then(() => cb(), cb)
-	}
 
-	const out = new Transform({
-		objectMode: true,
-		write: errorsWithRow('stop_times', processStopover),
-	})
-
-	;(async () => {
-		const [_services, _trips] = await Promise.all([
-			readServicesAndExceptions(readFile, timezone, filters),
-			readTrips(readFile, filters.trip),
-		])
-		services = _services
-		for await (const [id, trip] of _trips) {
-			await _trips.set(id, [trip.service_id, trip.route_id])
+			// headway-based
+			// todo: DRY with compute-connections
+			const t0 = arrs[0]
+			const hwStartsL = hwStarts ? hwStarts.length : 0
+			for (let h = 0; h < hwStartsL; h++) {
+				for (let t = hwStarts[h]; t < hwEnds[h]; t += hwHeadways[h]) {
+					for (let i = 0; i < stops.length; i++) {
+						const arr = t + arrs[i] - t0
+						const dep = t + deps[i] - t0
+						yield {
+							stop_id: stops[i],
+							trip_id: tripId,
+							service_id: serviceId,
+							route_id: routeId,
+							start_of_trip: day,
+							arrival: resolveTime(timezone, day, arr),
+							departure: resolveTime(timezone, day, dep),
+							headwayBased: true,
+						}
+					}
+				}
+			}
 		}
-		trips = _trips
-
-		pump(
-			readFile('stop_times'),
-			out,
-			(err) => {
-				if (err) out.destroy(err)
-			},
-		)
-	})()
-	.catch((err) => {
-		out.destroy(err)
-	})
-	return out
+	}
 }
 
 module.exports = computeStopoverTimes
