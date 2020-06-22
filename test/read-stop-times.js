f0efdb7a03798ea8cab1a3fc0c152d8ca1372be7
Jannis R <mail@jannisr.de>
2020-06-22 21:27:41 +0200

    read-stop-times as async generator :boom:

--- test/read-stop-times.js
+++ test/read-stop-times.js
@@ -85,31 +85,4 @@ test('read-stop-times works', async (t) => {
 			headwayBasedHeadways: []
 		},
 	])
-	t.deepEqual(Array.from(arrivalsByTripId.raw.entries()), [
-		['a-downtown-all-day', [55380, 55800, 56100]],
-		['a-outbound-all-day', [61980, 62400, 62700]],
-		['b-downtown-on-working-days', [32100, 32520, 33120, 47580, 48000, 48600]],
-		['b-downtown-on-weekends', [47580, 48120, 48600]],
-		['b-outbound-on-working-days', [65580, 66000, 66600]],
-		['b-outbound-on-weekends', [65580, 66120, 66600]],
-		['c-downtown-all-day', [55620, 55980]],
-	])
-	t.deepEqual(Array.from(departuresByTripId.raw.entries()), [
-		['a-downtown-all-day', [55440, 55860, 56160]],
-		['a-outbound-all-day', [62040, 62460, 62760]],
-		['b-downtown-on-working-days', [32160, 32640, 33180, 47640, 48120, 48660]],
-		['b-downtown-on-weekends', [47640, 48240, 48660]],
-		['b-outbound-on-working-days', [65640, 66120, 66660]],
-		['b-outbound-on-weekends', [65640, 66240, 66660]],
-		['c-downtown-all-day', [55680, 56100]],
-	])
-	t.deepEqual(Array.from(headwayBasedStarts.raw.entries()), [
-		['b-outbound-on-working-days', [54000]],
-	])
-	t.deepEqual(Array.from(headwayBasedEnds.raw.entries()), [
-		['b-outbound-on-working-days', [57600]],
-	])
-	t.deepEqual(Array.from(headwayBasedHeadways.raw.entries()), [
-		['b-outbound-on-working-days', [600]],
-	])
 })
