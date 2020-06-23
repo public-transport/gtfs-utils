70643f906672ad997fddaf4f12dc81db6cab42b5
Jannis R <mail@jannisr.de>
2020-06-23 21:51:57 +0200

    resolve-time: add LRU cache

--- benchmark.js
+++ benchmark.js
@@ -3,6 +3,7 @@
 const {Suite} = require('benchmark')
 const parseDate = require('./parse-date')
 const daysBetween = require('./lib/days-between')
+const resolveTime = require('./lib/resolve-time')
 
 const T0 = 1548975600000 // 2019-02-01T00:00+01:00
 
@@ -37,6 +38,17 @@ s.add('daysBetween: static arguments', () => {
 s.add('daysBetween: random start date', () => {
 	const startDate = randomStartDates[Math.random() * randomStartDates.length | 0]
 	daysBetween(startDate, '20200606', allWeekdays, 'Europe/Berlin')
+})
+
+const randomTimes = new Array(50).fill(null)
+.map(() => (Math.random() * 27 * 3600) | 0) // GTFS Time values can be >24h
+
+s.add('resolveTime: static arguments', () => {
+	resolveTime('Europe/Berlin', T0, 15 * 3600 + 30 * 60)
+})
+s.add('resolveTime: random time', () => {
+	const time = randomTimes[Math.random() * randomTimes.length | 0]
+	resolveTime('Europe/Berlin', T0, time)
 })
 
 s.on('error', (err) => {
