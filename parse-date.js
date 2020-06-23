2330a08aeed5f643c187f2acbe5b6cd3c59f30b6
Jannis R <mail@jannisr.de>
2020-06-23 20:42:15 +0200

    parse-date, days-between: add LRU cache

--- parse-date.js
+++ parse-date.js
@@ -1,8 +1,11 @@
 'use strict'
 
+const LRUCache = require('quick-lru')
 const {zonedTimeToUtc} = require('date-fns-tz')
 
 const dateFormat = /^\d{8}$/
+
+const cache = new LRUCache({maxSize: 200})
 
 const parseDate = (str, timezone) => {
 	if ('string' !== typeof str) throw new Error('str must be a string.')
@@ -11,13 +14,19 @@ const parseDate = (str, timezone) => {
 		throw new Error('timezone must be a non-empty string.')
 	}
 
+	const signature = str + '-' + timezone
+	if (cache.has(signature)) return cache.get(signature)
+
 	const isoDate = [
 		str.substr(0, 4),
 		str.substr(4, 2),
 		str.substr(6, 2)
 	].join('-')
 	const millis = +zonedTimeToUtc(isoDate + ' 00:00', timezone)
-	return millis / 1000 | 0 // make UNIX timestamp
+	const t = millis / 1000 | 0 // make UNIX timestamp
+
+	cache.set(signature, t)
+	return t
 }
 
 module.exports = parseDate
