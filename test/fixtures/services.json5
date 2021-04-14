public-transport/gtfs-utils

commit b21e97f049be0fcdad5322687945d5df14515d4e

lib/dates-between: Luxon -> date-fns ⚡️
before:
datesBetween: static arguments x 456,500 ops/sec ±0.20% (96 runs sampled)
datesBetween: random start date x 1,257 ops/sec ±195.37% (94 runs sampled)

after:
datesBetween: static arguments x 456,164 ops/sec ±0.30% (96 runs sampled)
datesBetween: random start date x 433,259 ops/sec ±0.40% (95 runs sampled)

lib/dates-between.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const LRUCache = require('quick-lru')
-const {DateTime} = require('luxon')
+const addDays = require('date-fns/addDays')
 
 const parseDate = require('./parse-date')
 
@@ -20,14 +20,13 @@ const noWeekdays = {
 const date = 24 * 60 * 60
 
 const weekdayIndexes = [
-	null,
+	'sunday',
 	'monday',
 	'tuesday',
 	'wednesday',
 	'thursday',
 	'friday',
 	'saturday',
-	'sunday'
 ]
 
 const cache = new LRUCache({maxSize: 50})
@@ -55,17 +54,17 @@ const computeDatesBetween = (beginning, end, weekdays, timezone) => {
 	].join('-')
 	if (cache.has(signature)) return cache.get(signature)
 
-	// todo: we're just generating dates, is there a faster way than Luxon?
-
 	beginning = parseDate(beginning)
 	end = parseDate(end)
-	end = +DateTime.fromISO(end + 'T00:00', {zone: timezone})
+	end = Date.parse(end + 'T00:00Z')
 
 	const dates = []
-	let t = DateTime.fromISO(beginning + 'T00:00', {zone: timezone})
+	let t = new Date(beginning + 'T00:00Z')
 	for (let i = 0; t <= end; i++) {
-		if (weekdays[weekdayIndexes[t.weekday]]) dates.push(t.toISODate())
-		t = t.plus({days: 1})
+		if (weekdays[weekdayIndexes[t.getDay()]]) {
+			dates.push(t.toISOString().slice(0, 10))
+		}
+		t = addDays(t, 1)
 	}
 
 	cache.set(signature, dates)

commit 1c5b7a4e388dc1f99e1d76a39b10c841dcf5b253

lib/dates-between: fix for non-UTC environments 🐛✅
This bug had been introduced by b21e97f.

lib/dates-between.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const LRUCache = require('quick-lru')
-const addDays = require('date-fns/addDays')
 
 const parseDate = require('./parse-date')
 
@@ -61,10 +60,10 @@ const computeDatesBetween = (beginning, end, weekdays, timezone) => {
 	const dates = []
 	let t = new Date(beginning + 'T00:00Z')
 	for (let i = 0; t <= end; i++) {
-		if (weekdays[weekdayIndexes[t.getDay()]]) {
+		if (weekdays[weekdayIndexes[t.getUTCDay()]]) {
 			dates.push(t.toISOString().slice(0, 10))
 		}
-		t = addDays(t, 1)
+		t.setUTCDate(t.getUTCDate() + 1)
 	}
 
 	cache.set(signature, dates)

test/fixtures/services.json5
@@ -1,0 +1,364 @@
+// All services (exceptions applied) in the sample-gtfs-feed@0.9.0 npm package.
+{
+	'all-day': [
+		'2019-03-01',
+		'2019-03-02',
+		'2019-03-03',
+		'2019-03-04',
+		'2019-03-05',
+		'2019-03-06',
+		'2019-03-07',
+		'2019-03-08',
+		'2019-03-09',
+		'2019-03-10',
+		'2019-03-11',
+		'2019-03-12',
+		'2019-03-13',
+		'2019-03-14',
+		'2019-03-15',
+		'2019-03-16',
+		'2019-03-17',
+		'2019-03-18',
+		'2019-03-19',
+		'2019-03-20',
+		'2019-03-21',
+		'2019-03-22',
+		'2019-03-23',
+		'2019-03-24',
+		'2019-03-25',
+		'2019-03-26',
+		'2019-03-27',
+		'2019-03-28',
+		'2019-03-29',
+		'2019-03-30',
+		'2019-03-31',
+
+		'2019-04-01',
+		'2019-04-02',
+		'2019-04-03',
+		'2019-04-04',
+		'2019-04-05',
+		'2019-04-06',
+		'2019-04-07',
+		'2019-04-08',
+		'2019-04-09',
+		'2019-04-10',
+		'2019-04-11',
+		'2019-04-12',
+		'2019-04-13',
+		'2019-04-14',
+		'2019-04-15',
+		'2019-04-16',
+		'2019-04-17',
+		'2019-04-18',
+		'2019-04-19',
+		'2019-04-20',
+		'2019-04-21',
+		'2019-04-22',
+		'2019-04-23',
+		'2019-04-24',
+		'2019-04-25',
+		'2019-04-26',
+		'2019-04-27',
+		'2019-04-28',
+		'2019-04-29',
+		'2019-04-30',
+
+		// exception: 2019-05-01
+		'2019-05-02',
+		'2019-05-03',
+		'2019-05-04',
+		'2019-05-05',
+		'2019-05-06',
+		'2019-05-07',
+		'2019-05-08',
+		'2019-05-09',
+		'2019-05-10',
+		'2019-05-11',
+		'2019-05-12',
+		'2019-05-13',
+		'2019-05-14',
+		'2019-05-15',
+		'2019-05-16',
+		'2019-05-17',
+		'2019-05-18',
+		'2019-05-19',
+		'2019-05-20',
+		'2019-05-21',
+		'2019-05-22',
+		'2019-05-23',
+		'2019-05-24',
+		'2019-05-25',
+		'2019-05-26',
+		'2019-05-27',
+		'2019-05-28',
+		'2019-05-29',
+		'2019-05-30',
+		'2019-05-31',
+
+		'2019-06-01',
+		'2019-06-02',
+		'2019-06-03',
+		'2019-06-04',
+		'2019-06-05',
+		'2019-06-06',
+		'2019-06-07',
+		'2019-06-08',
+		'2019-06-09',
+		'2019-06-10',
+		'2019-06-11',
+		'2019-06-12',
+		'2019-06-13',
+		'2019-06-14',
+		'2019-06-15',
+		'2019-06-16',
+		'2019-06-17',
+		'2019-06-18',
+		'2019-06-19',
+		'2019-06-20',
+		'2019-06-21',
+		'2019-06-22',
+		'2019-06-23',
+		'2019-06-24',
+		'2019-06-25',
+		'2019-06-26',
+		'2019-06-27',
+		'2019-06-28',
+		'2019-06-29',
+		'2019-06-30',
+
+		'2019-07-01',
+		'2019-07-02',
+		'2019-07-03',
+		'2019-07-04',
+		'2019-07-05',
+		'2019-07-06',
+		'2019-07-07',
+		'2019-07-08',
+		'2019-07-09',
+		'2019-07-10',
+		'2019-07-11',
+		'2019-07-12',
+		'2019-07-13',
+		'2019-07-14',
+		// exception: 2019-07-15
+		// exception: 2019-07-16
+		// exception: 2019-07-17
+		// exception: 2019-07-18
+		// exception: 2019-07-19
+		// exception: 2019-07-20
+		// exception: 2019-07-21
+		'2019-07-22',
+		'2019-07-23',
+		'2019-07-24',
+		'2019-07-25',
+		'2019-07-26',
+		'2019-07-27',
+		'2019-07-28',
+		'2019-07-29',
+		'2019-07-30',
+		'2019-07-31',
+
+		'2019-08-01',
+		'2019-08-02',
+		'2019-08-03',
+		'2019-08-04',
+		'2019-08-05',
+		'2019-08-06',
+		'2019-08-07',
+		'2019-08-08',
+		'2019-08-09',
+		'2019-08-10',
+		'2019-08-11',
+		'2019-08-12',
+		'2019-08-13',
+		'2019-08-14',
+		'2019-08-15',
+		'2019-08-16',
+		'2019-08-17',
+		'2019-08-18',
+		'2019-08-19',
+		'2019-08-20',
+	],
+
+	'during-dst': [
+		'2019-03-30',
+	],
+
+	'on-weekends': [
+		'2019-03-03',
+		'2019-03-09',
+		'2019-03-10',
+		'2019-03-16',
+		'2019-03-17',
+		'2019-03-23',
+		'2019-03-24',
+		'2019-03-30',
+		'2019-03-31',
+
+		'2019-04-06',
+		'2019-04-07',
+		'2019-04-13',
+		'2019-04-14',
+		'2019-04-20',
+		'2019-04-21',
+		'2019-04-27',
+		'2019-04-28',
+
+		'2019-05-04',
+		'2019-05-05',
+		'2019-05-11',
+		'2019-05-12',
+		'2019-05-18',
+		'2019-05-19',
+		'2019-05-25',
+		'2019-05-26',
+
+		'2019-06-01',
+		'2019-06-02',
+		'2019-06-08',
+		'2019-06-09',
+		'2019-06-15',
+		'2019-06-16',
+		'2019-06-22',
+		'2019-06-23',
+		'2019-06-29',
+		'2019-06-30',
+
+		'2019-07-06',
+		'2019-07-07',
+		'2019-07-13',
+		'2019-07-14',
+		// exception: 2019-07-20
+		// exception: 2019-07-21
+		'2019-07-27',
+		'2019-07-28',
+
+		'2019-08-03',
+		'2019-08-04',
+		'2019-08-10', // saturday
+	],
+
+	'on-working-days': [
+		'2019-03-08',
+		'2019-03-09',
+		'2019-03-11',
+		'2019-03-12',
+		'2019-03-13',
+		'2019-03-14',
+		'2019-03-15',
+		'2019-03-16',
+		'2019-03-18',
+		'2019-03-19',
+		'2019-03-20',
+		'2019-03-21',
+		'2019-03-22',
+		'2019-03-23',
+		'2019-03-25',
+		'2019-03-26',
+		'2019-03-27',
+		'2019-03-28',
+		'2019-03-29',
+		'2019-03-30',
+
+		'2019-04-01',
+		'2019-04-02',
+		'2019-04-03',
+		'2019-04-04',
+		'2019-04-05',
+		'2019-04-06',
+		'2019-04-08',
+		'2019-04-09',
+		'2019-04-10',
+		'2019-04-11',
+		'2019-04-12',
+		'2019-04-13',
+		'2019-04-15',
+		'2019-04-16',
+		'2019-04-17',
+		'2019-04-18',
+		'2019-04-19',
+		'2019-04-20',
+		'2019-04-22',
+		'2019-04-23',
+		'2019-04-24',
+		'2019-04-25',
+		'2019-04-26',
+		'2019-04-27',
+		'2019-04-29',
+		'2019-04-30',
+
+		// exception: 2019-05-01
+		'2019-05-02',
+		'2019-05-03',
+		'2019-05-04',
+		'2019-05-06',
+		'2019-05-07',
+		'2019-05-08',
+		'2019-05-09',
+		'2019-05-10',
+		'2019-05-11',
+		'2019-05-13',
+		'2019-05-14',
+		'2019-05-15',
+		'2019-05-16',
+		'2019-05-17',
+		'2019-05-18',
+		'2019-05-20',
+		'2019-05-21',
+		'2019-05-22',
+		'2019-05-23',
+		'2019-05-24',
+		'2019-05-25',
+		'2019-05-27',
+		'2019-05-28',
+		'2019-05-29',
+		'2019-05-30',
+		'2019-05-31',
+
+		'2019-06-01',
+		'2019-06-03',
+		'2019-06-04',
+		'2019-06-05',
+		'2019-06-06',
+		'2019-06-07',
+		'2019-06-08',
+		'2019-06-10',
+		'2019-06-11',
+		'2019-06-12',
+		'2019-06-13',
+		'2019-06-14',
+		'2019-06-15',
+		'2019-06-17',
+		'2019-06-18',
+		'2019-06-19',
+		'2019-06-20',
+		'2019-06-21',
+		'2019-06-22',
+		'2019-06-24',
+		'2019-06-25',
+		'2019-06-26',
+		'2019-06-27',
+		'2019-06-28',
+		'2019-06-29',
+
+		'2019-07-01',
+		'2019-07-02',
+		'2019-07-03',
+		'2019-07-04',
+		'2019-07-05',
+		'2019-07-06',
+		'2019-07-08',
+		'2019-07-09',
+		'2019-07-10',
+		'2019-07-11',
+		'2019-07-12',
+		'2019-07-13',
+		// exception: 2019-07-15
+		// exception: 2019-07-16
+		// exception: 2019-07-17
+		// exception: 2019-07-18
+		// exception: 2019-07-19
+		'2019-07-20',
+	],
+}

test/index.js
@@ -11,6 +11,7 @@ const formatDate = require('../format-date')
 const datesBetween = require('../lib/dates-between')
 const resolveTime = require('../lib/resolve-time')
 const readStopTimezones = require('../lib/read-stop-timezones')
+const readServicesAndExceptions = require('../read-services-and-exceptions')
 const computeStopovers = require('../compute-stopovers')
 const computeSortedConnections = require('../compute-sorted-connections')
 const computeServiceBreaks = require('../compute-service-breaks')
@@ -146,6 +147,15 @@ test('lib/read-stop-timezones', async (t) => {
 })
 
 require('./read-stop-times')
+
+const servicesFixtures = readJSON5Sync(require.resolve('./fixtures/services.json5'))
+test('read-services-and-exceptions: works', async (t) => {
+	const services = readServicesAndExceptions(readFile, 'Europe/Berlin')
+	const res = {}
+	for await (const [id, dates] of services) res[id] = dates
+
+	t.deepEqual(res, servicesFixtures)
+})
 
 const stopoversFixtures = readJSON5Sync(require.resolve('./fixtures/stopovers.json5'))
 test('compute-stopovers: works', async (t) => {