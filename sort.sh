03c043cf1dadd0d0f87b7b6ae7b05ee26f74d20c
Jannis R <mail@jannisr.de>
2021-05-23 00:05:22 +0100

    sort.sh: support .txt file ending 💥
    closes #37

--- sort.sh
+++ sort.sh
@@ -4,21 +4,23 @@ set -e
 set -o pipefail
 
 function sort() {
-	if [ -f "$1" ]; then
-		2>&1 echo "$ mlr --csv sort ${@:2} $1"
-		mlr --csv sort ${@:2} "$1" | sponge "$1"
+	if [ -f "$1.csv" ]; then local file="$1.csv"
+	elif [ -f "$1.txt" ]; then local file="$1.txt"
+	else return
 	fi
+	2>&1 echo "$ mlr --csv sort ${@:2} $file"
+	mlr --csv sort ${@:2} "$file" | sponge "$file"
 }
 
-sort agency.csv -f agency_id
+sort agency -f agency_id
 # For stations (loc_type 1), parent_station must be empty.
 # We get stations first by sorting on parent_station.
-sort stops.csv -f parent_station -nr location_type
-sort routes.csv -f route_id
-sort trips.csv -f trip_id
-sort stop_times.csv -f trip_id -n stop_sequence
-sort calendar.csv -f service_id
-sort calendar_dates.csv -f service_id,date
-sort shapes.csv -f shape_id -n shape_pt_sequence
+sort stops -f parent_station -nr location_type
+sort routes -f route_id
+sort trips -f trip_id
+sort stop_times -f trip_id -n stop_sequence
+sort calendar -f service_id
+sort calendar_dates -f service_id,date
+sort shapes -f shape_id -n shape_pt_sequence
 # todo: sort start_time properly (it may be HH:MM:SS or H:MM:SS)
-sort frequencies.csv -f trip_id,start_time
+sort frequencies -f trip_id,start_time
