public-transport/gtfs-utils

commit 8e0c03757da717fbfe556ab3426b2291d0232c95

make readFile async 💥

read-csv.js
@@ -6,7 +6,7 @@ const {pipeline} = require('stream')
 const stripBomStream = require('strip-bom-stream')
 const parseCsv = require('csv-parser')
 
-const readCsv = (path) => {
+const readCsv = async (path) => {
 	const isPathStream = isReadable(path)
 	if (typeof path !== 'string' && !isPathStream) {
 		throw new Error('path must be a string or a Readable stream')

test/index.js
@@ -65,16 +65,14 @@ const readFile = (file) => {
 const utc = 'Etc/UTC'
 const berlin = 'Europe/Berlin'
 
-test('read-csv: accept a readable stream as input', (t) => {
+test('read-csv: accept a readable stream as input', async (t) => {
 	const readable = createReadStream(require.resolve('sample-gtfs-feed/gtfs/stops.txt'))
-	const src = readCsv(readable)
+	const src = await readCsv(readable)
 
-	src.once('data', (stop) => {
-		t.ok(stop)
-		t.ok(stop.stop_id)
-		src.destroy()
-		t.end()
-	})
+	const stop = await new Promise(res => src.once('data', res))
+	t.ok(stop)
+	t.ok(stop.stop_id)
+	src.destroy()
 })
 
 test('format-date', (t) => {

examples/read-stops.js
@@ -3,8 +3,8 @@
 const readCsv = require('../read-csv')
 const readStops = require('../read-stops')
 
-const readFile = (file) => {
-	return readCsv(require.resolve('sample-gtfs-feed/gtfs/' + file + '.txt'))
+const readFile = async (file) => {
+	return await readCsv(require.resolve('sample-gtfs-feed/gtfs/' + file + '.txt'))
 }
 
 ;(async () => {

lib/read-stop-stations.js
@@ -12,7 +12,7 @@ const readStopStations = async (readFile, filters, createStore) => {
 	}
 
 	const stations = createStore() // station ID by stop_id
-	for await (const s of readFile('stops')) {
+	for await (const s of await readFile('stops')) {
 		if (!filters.stop(s)) continue
 
 		const stationId = s.parent_station

lib/read-stop-times.js
@@ -21,13 +21,13 @@ const readStopTimes = async function* (readFile, filters) {
 
 	// todo: DRY with read-trips.js
 
-	const trips = readFile('trips')
+	const trips = await readFile('trips')
 	const checkTripsSorting = expectSorting('trips', (a, b) => {
 		if (a.trip_id === b.trip_id) return 0
 		return a.trip_id < b.trip_id ? -1 : 1
 	})
 
-	const stopTimes = readFile('stop_times')
+	const stopTimes = await readFile('stop_times')
 	const compareStopTimes = (trip, stopTime) => {
 		if (trip.trip_id === stopTime.trip_id) return 0
 		return trip.trip_id < stopTime.trip_id ? -1 : 1
@@ -48,7 +48,7 @@ const readStopTimes = async function* (readFile, filters) {
 		[Symbol.asyncIterator]: async function* () {},
 	})
 	try {
-		const frequencies = readFile('frequencies')
+		const frequencies = await readFile('frequencies')
 		const compareFrequencies = (trip, freq) => {
 			if (trip.trip_id === freq.trip_id) return 0
 			return trip.trip_id < freq.trip_id ? -1 : 1

lib/read-stop-timezones.js
@@ -14,7 +14,7 @@ const readStopTimezones = async (readFile, filters, createStore) => {
 	}
 
 	const tzs = createStore() // station/stop timezone, by stop_id
-	for await (const s of readFile('stops')) {
+	for await (const s of await readFile('stops')) {
 		if (
 			('location_type' in s)
 			&& !['', STOP, STATION].includes(s.location_type)

read-pathways.js
@@ -36,7 +36,7 @@ const readPathways = async function* (readFile, filters = {}, opt = {}) {
 	const pathways = createStore() // by node/stop ID
 	const pathwaysByFrom = createStore() // pathway IDs by from_stop_id
 
-	for await (let pw of readFile('pathways')) {
+	for await (let pw of await readFile('pathways')) {
 		if (!pathwayFilter(pw)) continue
 		if (
 			!pw.pathway_id

read-services-and-exceptions.js
@@ -33,13 +33,13 @@ const readServicesAndExceptions = async function* (readFile, timezone, filters =
 		throw new TypeError('filters.serviceException must be a function')
 	}
 
-	const services = readFile('calendar')
+	const services = await readFile('calendar')
 	const checkServicesSorting = expectSorting('calendar', (a, b) => {
 		if (a.service_id === b.service_id) return 0
 		return a.service_id < b.service_id ? -1 : 1
 	})
 
-	const exceptions = readFile('calendar_dates')
+	const exceptions = await readFile('calendar_dates')
 	const compareException = (svc, ex) => {
 		if (svc.service_id === ex.service_id) return 0
 		return svc.service_id < ex.service_id ? -1 : 1

commit 33eb370893c31054fc6bd9fac694ebfa295593ef

read-csv: reject on fs errors 🐛✅
This bug was introduced by 8e0c037.

read-csv.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const {readable: isReadable} = require('is-stream')
-const fs = require('fs')
+const {createReadStream} = require('fs')
 const {pipeline} = require('stream')
 const stripBomStream = require('strip-bom-stream')
 const parseCsv = require('csv-parser')
@@ -11,9 +11,34 @@ const readCsv = async (path) => {
 	if (typeof path !== 'string' && !isPathStream) {
 		throw new Error('path must be a string or a Readable stream')
 	}
+	const src = isPathStream ? path : createReadStream(path)
+
+	if (!isPathStream) {
+		// When consuming an fs.createReadStream readable stream of a non-
+		// existent file via `for await`, it only emits the `ENOENT` error at
+		// the first iteration. We want to fail right away though, so we listen
+		// for `readable` & `error` here.
+		// This sets the stream to flowing/resumed mode, but since we pipe it
+		// into another stream right after, that's fine.
+		// see also https://nodejs.org/docs/latest-v14.x/api/stream.html#stream_two_reading_modes
+		await new Promise((resolve, reject) => {
+			const onError = (err) => {
+				reject(err)
+				src.removeListener('error', onError)
+				src.removeListener('readable', onReadable)
+			}
+			const onReadable = () => {
+				resolve()
+				src.removeListener('error', onError)
+				src.removeListener('readable', onReadable)
+			}
+			src.on('error', onError)
+			src.on('readable', onReadable)
+		})
+	}
 
 	return pipeline(
-		isPathStream ? path : fs.createReadStream(path),
+		src,
 		stripBomStream(),
 		parseCsv(),
 		() => {}, // no-op cb

test/index.js
@@ -76,6 +76,17 @@ test('read-csv: accept a readable stream as input', async (t) => {
 	src.destroy()
 })
 
+test('read-csv: rejects on ENOENT', async (t) => {
+	let rejected = false
+	const p = readCsv(pathJoin(__dirname, '_non-existent_'))
+	await p.catch((err) => {
+		rejected = true
+		t.ok(err, 'err')
+		t.equal(err.code, 'ENOENT', 'err.code')
+	})
+	t.equal(rejected, true, 'did not reject')
+})
+
 test('format-date', (t) => {
 	t.plan(3)
 	t.equal(formatDate(1551571200, utc), '20190303')